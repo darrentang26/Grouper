a3ebcd9dcd36066dc06a758ed5b74692e7f79c71
darrentang26 <darren.tang@tufts.edu>
2022-02-25 14:04:48 -0500

    Added functions to convert ast nodes to strings

--- src/toplevel.ml
+++ src/toplevel.ml
@@ -1,5 +1,5 @@
 let () =
-  let usage_msg = "usage: ./toplevel.native [file.mc]" in
+  let usage_msg = "usage: ./toplevel.native [file.grp]" in
   let channel = ref stdin in
     Arg.parse [] (fun file -> channel := open_in file) usage_msg;
 let lexbuf = Lexing.from_channel !channel in
