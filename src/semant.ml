d949227ae337e208603e13c77e55e5fee8a6807a
Robby Nooney <robbynooney@gmail.com>
2022-03-25 15:50:40 -0700

    started semant

---
+++ src/semant.ml
@@ -0,0 +1,26 @@
+(* Semantic Analysis *)
+
+open Ast
+open Sast
+
+module StringMap = Map.Make(String)
+
+
+let pre_check (types, letb) = match letb with
+    (Let (bs, e)) -> check ((Let (bs, e)))
+    _ -> raise (Failure "illegal toplevel expression")
+
+let check = function
+
+let lookup_type name Gamma = 
+    try StringMap.find name Gamma
+        with Not_found -> raise (Failure, "unbound identifier " ^ name)
+
+let rec Gamma expr = function
+        Literal  l  -> (Int, SLiteral l)
+      | Fliteral l  -> (Float, SFliteral l)
+      | BoolLit l   -> (Bool, SBoolLit l)
+      | StringLit l -> (String, SStringLit l)
+      | Name s      -> (lookup_type s Gamma, SName s)
+      | 
+
