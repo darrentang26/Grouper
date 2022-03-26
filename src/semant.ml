48dc7257d077c691cac36b2ab041b62d8d96ffe7
Michael Simpson <michael.simpson@tufts.edu>
2022-03-26 12:32:22 -0400

    add draft of pair and list expressions

--- src/semant.ml
+++ src/semant.ml
@@ -16,11 +16,27 @@ let lookup_type name Gamma =
     try StringMap.find name Gamma
         with Not_found -> raise (Failure, "unbound identifier " ^ name)
 
+let type_eq ty1 ty2 = function
+
 let rec Gamma expr = function
         Literal  l  -> (Int, SLiteral l)
       | Fliteral l  -> (Float, SFliteral l)
       | BoolLit l   -> (Bool, SBoolLit l)
       | StringLit l -> (String, SStringLit l)
+      | PairExpr (e1, e2) -> let
+            (t1, node1) = Gamma e1 and
+            (t2, node2) = Gamma e2
+                in (PairType (t1, t2), SPairExpr (node1, node2))
+      | ListExpr es -> let
+            (ts, nodes) = List.split (List.map Gamma es) and
+            (* check if list is empty first *)
+            hd = List.hd ts and
+            _ = List.map
+                    (fun (t) -> if ~type_eq hd t 
+                        then raise (Failure, "lists must have the same type")
+                        else false)
+                    ts
+                in (ListType hd, SListExpr List.map Gamma nodes)
       | Name s      -> (lookup_type s Gamma, SName s)
       | 
 
