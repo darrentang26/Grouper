1e071c90e26c7a92ec717e903499cb1a5aba60a1
Michael Simpson <michael.simpson@tufts.edu>
2022-03-27 21:55:34 -0400

    Add cons lists to sast and semant them

--- src/semant.ml
+++ src/semant.ml
@@ -14,7 +14,7 @@ let check = function
 
 let lookup_type name Gamma = 
     try StringMap.find name Gamma
-        with Not_found -> raise (Failure, "unbound identifier " ^ name)
+        with Not_found -> raise (Failure "unbound identifier " ^ name)
 
 let type_eq ty1 ty2 = function
 
@@ -24,19 +24,24 @@ let rec semant Gamma expr = function
       | BoolLit l   -> (Bool, SBoolLit l)
       | StringLit l -> (String, SStringLit l)
       | PairExpr (e1, e2) -> let
-            (t1, node1) = semant e1 and
-            (t2, node2) = semant e2
+            (t1, node1) = semant Gamma e1 and
+            (t2, node2) = semant Gamma e2
                 in (PairType (t1, t2), SPairExpr (node1, node2))
-      | ListExpr es -> let
-            (ts, nodes) = List.split (List.map semant es) and
-            (* check if list is empty first *)
-            hd = List.hd ts and
-            _ = List.map
-                    (fun (t) -> if ~type_eq hd t 
-                        then raise (Failure, "lists must have the same type")
-                        else false)
-                    ts
-                in (ListType hd, SListExpr List.map semant nodes)
+      | ConsExpr (expr, EmptyListExpr) -> let
+            (t, sexpr) = semant Gamma expr
+                in match t with
+                    | ListType EmptyListType -> raise (Failure "cannot hae a list of empty lists")
+                    | ListType t' -> (t, sexpr) 
+                    | _ -> (ListType t, SConsExpr (sexpr, EmptyListExpr))
+      | ConsExpr (e1, e2) -> let
+            (t1, s1) = semant Gamma e1 and
+            (t2, s2) = semant Gamma e2
+                in match t2 with
+                    | ListType t2' -> if type_eq t1 t2'
+                            then (t2, SConsExpr (s1, s2))
+                            else raise (Failure "must cons " ^ string_of_type_expr t1 ^ " onto a list of the same type, not " ^ string_of_type_expr t2)
+                    | _ -> raise (Failure "must cons onto a list type, not " ^ string_of_type_expr t2)
+      | EmptyListExpr -> (EmptyListType, SEmptyListExpr)
       | Name s      -> (lookup_type s Gamma, SName s)
       | 
 
