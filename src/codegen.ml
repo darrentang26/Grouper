105617977f0e439cd842bc1c3894af57567de134
Michael Simpson <michael.simpson@tufts.edu>
2022-03-30 22:22:14 -0400

    generate if statements

--- src/codegen.ml
+++ src/codegen.ml
@@ -31,42 +31,53 @@ let translate ((* types *) _, letb) =
     | None -> ignore (instr builder) in
 
   let rec expr builder ((t,e) : sexpr) = match e with
-    SLiteral i -> L.const_int i32_t i 
+    SLiteral i -> L.const_int i32_t i
   | SBoolLit b -> L.const_int i1_t (if b then 1 else 0)
   | SFliteral l -> L.const_float_of_string float_t l
-  | SStringLit str -> L.const_stringz context str
+  | SStringLit str -> L.build_global_stringptr str "" builder
   | SPrint sexpr -> (match sexpr with
       (StringExpr, sexpr') -> let
         value = expr builder (StringExpr, sexpr') 
-          in let sval = match (L.string_of_const value) with
+          (* in let sval = match (L.string_of_const value) with
                           Some s -> s
-                        | None -> ""
-          in let str = L.build_in_bounds_gep (L.build_global_stringptr sval "" builder) [| (L.const_int i32_t 0) |] "" builder
+                        | None -> "" *)
+          in let sval = value
+          in let str = L.build_in_bounds_gep sval [| (L.const_int i32_t 0) |] "" builder
           in let _ = L.build_call print_func [| str |] "printf" builder
             in value
     | _ -> raise (Failure "not yet implemented-- print only expects strings"))
   | SLet ((*binds*) _, body) -> (* Ignores bindings for now, just builds the body basic block *)
       expr builder body
-  | SIf cond_sexpr then_sexpr else_sexpr ->
-      let cond_value = expr builder cond_sexpr and
+  | SIf (cond_sexpr, then_sexpr, else_sexpr) ->
+    let cond_value = expr builder cond_sexpr
+    in let start_bb = L.insertion_block builder
+    in let the_function = L.block_parent start_bb
 
-      (* create the basic block for combining the branches *)
-      merge_bb = L.append_block context "merge" main_defn
-      in let branch_instr = L.build_br merge_bb and
+    in let then_bb = L.append_block context "then" the_function
+    in let _ = L.position_at_end then_bb builder
+    in let then_value = expr builder then_sexpr
+    in let then_bb = L.insertion_block builder
 
-      (* create the basic block for the then branch *)
-      then_bb = L.append_block context "then" main_defn
-      in let then_builder = expr (L.builder_at_end context then_bb) then_sexpr
-      in let () = L.add_terminal then_builder branch_instr and
+    in let else_bb = L.append_block context "else" the_function
+    in let _ = L.position_at_end else_bb builder
+    in let else_value = expr builder else_sexpr
+    in let else_bb = L.insertion_block builder
 
-      (* create the basic block for the else branch *)
-      else_bb = L.append_block context "else" main_defn
-      in let else_builder = expr (L.builder_at_end context else_bb) else_sexpr
-      in let () = L.add_terminal else_builder branch_instr in 
+    in let merge_bb = L.append_block context "ifcont" the_function
+    in let _ = L.position_at_end merge_bb builder
+    in let incoming = [(then_value, then_bb); (else_value, else_bb)]
+    in let phi = L.build_phi incoming "iftmp" builder
 
-      let _ = L.build_cond_br cond_value then_bb else_bb builder
-        in L.builder_at_end context merge_bb
-    
+    in let _ = L.position_at_end start_bb builder
+    in let _ = L.build_cond_br cond_value then_bb else_bb builder
+
+    in let _ = L.position_at_end then_bb builder
+    in let _ = L.build_br merge_bb builder
+
+    in let _ = L.position_at_end else_bb builder
+    in let _ = L.build_br merge_bb builder
+
+    in let _ = L.position_at_end merge_bb builder in phi
   | _ -> raise (Failure ("sexpr " ^ (Sast.string_of_sexpr (t,e)) ^ " not yet implemented"))
 in let _ = expr main_builder letb
 in let _ = L.build_ret (L.const_int i32_t 0) main_builder
