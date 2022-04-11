416e8c0e121ee650911bde0b1f9d9e2a22660174
Michael Simpson <michael.simpson@tufts.edu>
2022-04-10 23:02:33 -0400

    add lifting step

--- src/toplevel.ml
+++ src/toplevel.ml
@@ -1,5 +1,5 @@
 
-   type action = Ast | Sast | LLVM_IR | Compile
+   type action = Ast | Sast | Lifting | LLVM_IR | Compile
 
    let () =
      let action = ref Compile in
@@ -7,6 +7,7 @@
      let speclist = [
        ("-a", Arg.Unit (set_action Ast), "Print the AST");
        ("-s", Arg.Unit (set_action Sast), "Print the SAST");
+       ("-f", Arg.Unit (set_action Lifting), "Print the Lifted SAST");
        ("-l", Arg.Unit (set_action LLVM_IR), "Print the generated LLVM IR");
        ("-c", Arg.Unit (set_action Compile),
          "Check and print the generated LLVM IR (default)");
@@ -23,6 +24,17 @@
        match !action with
          Ast     -> ()
        | Sast    -> print_string (Sast.string_of_sprogram sast)
+       | Lifting ->
+            (match sast with (typ_decls, sexpr) ->
+              (* let lifted_sexpr = (Lifting.lift_free (Lifting.name_all (Lifting.mark_free sexpr [])))
+                in print_string (Sast.string_of_sprogram
+                  (typ_decls,
+                  lifted_sexpr)) *)
+              let lifted_sexpr = (Lifting.lift_free (Lifting.name_all (Lifting.mark_free sexpr [])))
+              in let (sexpr, functions) = Lifting.lift_functions lifted_sexpr
+                (* in print_string (Sast.string_of_sprogram (typ_decls, lifted_sexpr)) *)
+                in print_string (Sast.string_of_sprogram_lifted (typ_decls, functions, sexpr))
+            )
        | LLVM_IR -> print_string (Llvm.string_of_llmodule (Codegen.translate sast))
        | Compile -> let m = Codegen.translate sast in
      Llvm_analysis.assert_valid_module m;
